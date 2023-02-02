624a5285f7185811c779691997af27113146c212
DenizUyan <117668368+DenizUyan@users.noreply.github.com>
2023-02-02 21:18:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] list = {1,2,3,4,5,6,7};
+        double sum = 0.0;
+
+                for(int i = 0; i < list.length; i++){
+                    sum += list[i];
+                }
+
+                double ort = sum / list.length;
+
+        System.out.println("Sonuc: "+ ort);
+
+    }
+}
